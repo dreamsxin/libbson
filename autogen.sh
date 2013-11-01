c9c6f799fac2a6365a2d8588aa2734d498d47d1d
Christian Hergert <christian@hergert.me>
2013-11-01 15:17:46 -0700

    doc: use gtk-doc to generate API documentation.

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
+if GTKDOCIZE=$(which gtkdocize 2>/dev/null); then
+         gtkdocize --docdir doc/
+         gtkdocargs=--enable-gtk-doc
+else
+        echo "You don't have gtk-doc installed, and thus won't be able to generate the documentation."
+        echo 'EXTRA_DIST =' > doc/gtk-doc.make
+fi
+
+args="$gtkdocargs --enable-maintainer-flags"
+
 autoreconf -fiv
-./configure $@
+./configure $args "$@"
